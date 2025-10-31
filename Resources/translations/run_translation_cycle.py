9d738e6da5d9344e4e730d5a36e41d0a7ffdf56e
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-10-31 01:21:21 -0400

    Revert copyright header removal
    Waiting on attorneys answer.

--- Resources/translations/run_translation_cycle.py
+++ Resources/translations/run_translation_cycle.py
@@ -1,4 +1,25 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2025 The FreeCAD project association AISBL              *
+# *                                                                         *
+# *   This file is part of the FreeCAD Addon Manager.                       *
+# *                                                                         *
+# *   This is free software: you can redistribute it and/or modify it       *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   The software is distributed in the hope that it will be useful, but   *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with the FreeCAD Addon Manager. If not, see             *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
 
 """Run a full CrowdIn translation cycle for the Addon Manager. Requires that the
 CrowdIn API token is stored in ~/.crowdin-freecad-token, and that lupdate be in PATH."""
--- addonmanager_macro.py
+++ addonmanager_macro.py
@@ -1,5 +1,26 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2022-2023 FreeCAD Project Association                   *
+# *   Copyright (c) 2018 Gaël Écorchard <galou_breizh@yahoo.fr>             *
+# *                                                                         *
+# *   This file is part of FreeCAD.                                         *
+# *                                                                         *
+# *   FreeCAD is free software: you can redistribute it and/or modify it    *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful, but        *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with FreeCAD. If not, see                               *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
 
 """Unified handler for FreeCAD macros that can be obtained from different sources."""
 import base64
