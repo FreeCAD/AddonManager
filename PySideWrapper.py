b74592ee21a80e5865219fa44dbc1803dfd2c9ec
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-02-01 02:57:39 -0500

    Use corrected SPDX file copyright text

--- PySideWrapper.py
+++ PySideWrapper.py
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileCopyrightText: 2025 FreeCAD Project Association
 # SPDX-FileNotice: Part of the AddonManager.
-
 # ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2025 The FreeCAD project association AISBL              *
-# *                                                                         *
-
 # *   FreeCAD is free software: you can redistribute it and/or modify it    *
 # *   under the terms of the GNU Lesser General Public License as           *
 # *   published by the Free Software Foundation, either version 2.1 of the  *
